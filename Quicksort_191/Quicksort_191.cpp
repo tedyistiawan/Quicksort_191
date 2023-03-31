58205e6ec382bc12b22e07ca425788ac0126f4c7
tedyistiawan <tedypnd07@gmail.com>
2023-03-31 17:10:41 +0700

    Add project files.

---
+++ Quicksort_191/Quicksort_191.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+//array of integres to hold values
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of movement 
+int n;
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n maksimum panjang array adalah 20" << endl;
+	}
+
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + i) << ">";
+		cin >> arr[i];
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
