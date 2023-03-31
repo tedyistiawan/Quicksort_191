69e630fa52e18fccee297fc35a6efa6f204ea4fa
tedyistiawan <tedypnd07@gmail.com>
2023-03-31 17:28:18 +0700

    asli 8

--- Quicksort_191/Quicksort_191.cpp
+++ Quicksort_191/Quicksort_191.cpp
@@ -111,7 +111,14 @@ void display () {
 	cout << "Number of data movements: " << mov_count << endl;
 }
 
-int main() 
+int main() {
+	input();
+	q_short(0, n - 1);          // Sort the array using quick sort 
+	display();
+	system("pause");
+
+	return 0;
+}
 
 
 
